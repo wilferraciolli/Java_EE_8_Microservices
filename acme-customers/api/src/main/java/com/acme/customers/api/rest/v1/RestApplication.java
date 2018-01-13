4ca919b33e44f39a8de194741ae97efb1a7418b4
Wiliam Ferraciolli <wiliam334@hotmail.com>
2018-01-13 14:24:27 +0000

    Created the first end point. After the war file is packaged it can be deployed using payara-micro java -jar ~/dev/AppServer/payara-micro-4.1.2.174.jar --deploy api/target/acme-customers-api-1.0.0-SNAPSHOT.war

--- acme-customers/api/src/main/java/com/acme/customers/api/rest/v1/RestApplication.java
+++ acme-customers/api/src/main/java/com/acme/customers/api/rest/v1/RestApplication.java
@@ -4,7 +4,9 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * The type Rest application. Tis is the main starting point for every resource within this application.
+ * This is the main starting point for every resource within this application.
+ * To run this application, run te payara-micro jar and deploy this WAR file.
+ * Eg java -jar ~/dev/AppServer/payara-micro-4.1.2.174.jar --deploy api/target/acme-customers-api-1.0.0-SNAPSHOT.war
  */
 @ApplicationPath("/v1")
 public class RestApplication extends Application {
